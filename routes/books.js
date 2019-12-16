9932f793c997fa345a77288a7c3d4b02681216a5
Muhammad Gifar Zaini <gifarz06@gmail.com>
2019-12-16 10:12:47 +0700

    first commit

---
+++ routes/books.js
@@ -0,0 +1,121 @@
+var model = require("../models/index");
+
+module.exports = function(app) {
+  /* GET book listing. */
+  app.get("/books", function(req, res, next) {
+    model.Book.findAll({})
+      .then(books =>
+        res.json({
+          error: false,
+          data: books
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          data: [],
+          error: error
+        })
+      );
+  });
+
+  /* GET by ID book listing. */
+  app.get("/books/:id", function(req, res, next) {
+    model.Book.findOne()
+      .then(books =>
+        res.json({
+          error: false,
+          data: books
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          data: [],
+          error: error
+        })
+      );
+  });
+
+  /* POST book. */
+  app.post("/books", function(req, res, next) {
+    const { title, author, pages, language } = req.body;
+    model.Book.create({
+      title: title,
+      author: author,
+      pages: pages,
+      language: language
+    })
+      .then(book =>
+        res.status(201).json({
+          error: false,
+          data: book,
+          message: "New book has been created."
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          data: [],
+          error: error
+        })
+      );
+  });
+
+  /* update book. */
+  app.put("/books/:id", function(req, res, next) {
+    const book_id = req.params.id;
+
+    const { title, author, pages, language } = req.body;
+
+    model.Book.update(
+      {
+        title: title,
+        author: author,
+        pages: pages,
+        language: language
+      },
+      {
+        where: {
+          id: book_id
+        }
+      }
+    )
+      .then(book =>
+        res.json({
+          error: false,
+          message: "book has been updated."
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          error: error
+        })
+      );
+  });
+
+  /* GET book listing. */
+  /* Delete book. */
+  app.delete("/books/:id", function(req, res, next) {
+    const book_id = req.params.id;
+
+    model.Book.destroy({
+      where: {
+        id: book_id
+      }
+    })
+      .then(status =>
+        res.json({
+          error: false,
+          message: "book has been delete."
+        })
+      )
+      .catch(error =>
+        res.json({
+          error: true,
+          error: error
+        })
+      );
+  });
+};
